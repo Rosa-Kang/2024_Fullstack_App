c2440c3ff7b31b9e26a29d15e48e885b9959f160
Rosa Kang <therosessom@gmail.com>
2024-08-06 15:55:26 -0700

    update Header equivalent to the pages

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,13 +4,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User"],
+    tagTypes: ["User", "Products"],
     endpoints: (build) => ({
         getUser: build.query({
           query: (id) => `general/user/${id}`,
           providesTags: ["User"],
         }),
+        getProduct: build.query({
+          query: () => "general/products",
+          providesTags: ["Products"]
+        }),
     })
 })
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
