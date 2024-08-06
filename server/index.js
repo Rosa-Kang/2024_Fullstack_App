3955b4886518ceebf6ba0fa160005a7aac7bf11a
Rosa Kang <therosessom@gmail.com>
2024-08-06 11:50:37 -0700

    update route to getUse 2:08:39

--- server/index.js
+++ server/index.js
@@ -40,6 +40,6 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
-    User.insertMany(dataUser);
+    // User.insertMany(dataUser);
 
 }).catch((error) => console.log(`${error} did not connect.`));
