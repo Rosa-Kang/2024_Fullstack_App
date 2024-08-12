dd66a497c850037ed64c0680980d7736543df783
Rosa Kang <therosessom@gmail.com>
2024-08-12 11:52:27 -0700

    add a route to getAdmin in server 05:37:51

--- server/models/OverallStat.js
+++ server/models/OverallStat.js
@@ -7,14 +7,23 @@ const OverallStatSchema = new mongoose.Schema(
       yearlyTotalSoldUnits: Number,
       year: Number,
       monthlyData: [
-            { month: String, totalSales: Number, totalUnits: Number }
-        ],
+        {
+          month: String,
+          totalSales: Number,
+          totalUnits: Number,
+        },
+      ],
       dailyData: [
-            { date: String, totalSales: Number, totalUnits: Number },
-        ],
-       salesByCategory: [
-            { type: Map, of: Number,},
-        ],
+        {
+          date: String,
+          totalSales: Number,
+          totalUnits: Number,
+        },
+      ],
+      salesByCategory: {
+        type: Map,
+        of: Number,
+      },
     },
     { timestamps: true }
 );
